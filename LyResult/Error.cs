bd6dd9297182c0b36117a89886014f1a937a104c
admin <admin@lytraining2016.tk>
2026-03-17 11:36:56 +0300

    Add project files.

---
+++ LyResult/Error.cs
@@ -0,0 +1,31 @@
+﻿namespace LyResult;
+
+public record Error
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null.");
+
+    public Error(string code, string description)
+    {
+        Code = code;
+        Description = description;
+    }
+
+    public string Code { get; }
+    public string Description { get; }
+
+    public static Error NotFound(string description) =>
+        new("Error.NotFound", description);
+
+    public static Error Validation(string description) =>
+        new("Error.Validation", description);
+
+    public static Error Conflict(string description) =>
+        new("Error.Conflict", description);
+
+    public static Error Unauthorized(string description = "You are not authorized") =>
+        new("Error.Unauthorized", description);
+
+    public static Error Forbidden(string description = "You don't have permission") =>
+        new("Error.Forbidden", description);
+}
---
+++ LyResult/Extensions.cs
@@ -0,0 +1,45 @@
+﻿namespace LyResult;
+
+public static class Extensions
+{
+    public static Result<T> Ensure<T>(this Result<T> result,
+        Func<T, bool> predicate, Error error)
+    {
+        if (result.IsFailure)
+            return result;
+
+        return predicate(result.Value)
+            ? result
+            : Result.Failure<T>(error);
+    }
+
+    public static Result<T> Map<T>(this Result<T> result,
+        Action<T> action)
+    {
+        if (result.IsSuccess)
+            action(result.Value);
+
+        return result;
+    }
+
+    public static async Task<Result<T>> Bind<T, TPrevious>(
+        this Result<TPrevious> result,
+        Func<TPrevious, Task<Result<T>>> func)
+    {
+        if (result.IsFailure)
+            return Result.Failure<T>(result.Error);
+
+        return await func(result.Value);
+    }
+
+    public static Result Combine(params Result[] results)
+    {
+        foreach (var result in results)
+        {
+            if (result.IsFailure)
+                return result;
+        }
+
+        return Result.Success();
+    }
+}
